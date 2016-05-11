82aa3f2486d7369e5377b9b99dc43f08f03b6989
luzhiyong <luzy@2345.com>
2016-05-11 09:08:25 +0800

    备忘录模式

---
+++ MementoPattern/MementoPattern/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MementoPattern
+//
+//  Created by luzhiyong on 16/5/10.
+//  Copyright © 2016年 2345. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
