3ffc948127bf41eb68a2c45d55b96b4bbf9eddf7
zhaox <zhaox@2345.com>
2016-05-13 11:17:54 +0800

    8建造者模式

---
+++ 8Builder/8Builder/FDPresionBuilder.swift
@@ -0,0 +1,18 @@
+//
+//  FDPresionBuilder.swift
+//  8建造者模式
+//
+//  Created by Rainy on 16/5/13.
+//  Copyright © 2016年 Rainy. All rights reserved.
+//
+
+import Foundation
+
+protocol FDPresionBuilder {
+    func buildHead()
+    func buildBody()
+    func buildArmLeft()
+    func buildArmRight()
+    func buildLegLeft()
+    func buildLegRight()
+}
